740e3651105f51723ee9170d2d45af9814fd3e86
tinchin40 <tinchin40@gmail.com>
2022-04-28 14:34:22 +0800

    qanda下拉做好

---
+++ JS/qanda.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#q1').click(function () {
+        $('#a1').fadeToggle();
+    })
+    $('#q2').click(function () {
+        $('#a2').fadeToggle();
+    })
+    $('#q3').click(function () {
+        $('#a3').fadeToggle();
+    })
+    $('#q4').click(function () {
+        $('#a4').fadeToggle();
+    })
+    $('#q5').click(function(){
+        $('#a5').fadeToggle();
+    })
+    $('#q6').click(function(){
+        $('#a6').fadeToggle()
+    })
+    $('#q7').click(function(){
+        $('#a7').fadeToggle()
+    })
+
+});
