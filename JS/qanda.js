dd357a918ad84ad079503a0c925118fcc6325ff2
tinchin40 <tinchin40@gmail.com>
2022-05-03 02:54:12 +0800

    qa選單ok qa的414rwd完成

--- JS/qanda.js
+++ JS/qanda.js
@@ -1,24 +1,7 @@
 $(document).ready(function () {
-    $('#q1').click(function () {
-        $('#a1').fadeToggle(300);
+    $('.qa_up').click(function () {
+        $(this).parent().find('.qa_down').slideToggle(300);
+        $(this).parent().siblings().find('.qa_down').slideUp(300)
     })
-    $('#q2').click(function () {
-        $('#a2').fadeToggle(300);
-      })
-    $('#q3').click(function () {
-        $('#a3').fadeToggle(300);
-    })
-    $('#q4').click(function () {
-        $('#a4').fadeToggle(300);
-    })
-    $('#q5').click(function(){
-        $('#a5').fadeToggle(300);
-    })
-    $('#q6').click(function(){
-        $('#a6').fadeToggle(300)
-    })
-    $('#q7').click(function(){
-        $('#a7').fadeToggle(300)
-    })
-
+    
 });
