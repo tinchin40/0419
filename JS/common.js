baed543f6085faa99da611b9bb25124066628bad
tinchin40 <tinchin40@gmail.com>
2022-05-02 22:18:18 +0800

    QA 414RWD完成

---
+++ JS/common.js
@@ -0,0 +1,8 @@
+// 漢堡
+$(document).ready(function () {
+
+    $('#ham').click(function () {
+        $('li').slideToggle(300);
+    })
+
+})
--- JS/qanda.js
+++ JS/qanda.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
     $('#q1').click(function () {
-        $('#a1').fadeToggle();
+        $('#a1').fadeToggle(300);
     })
     $('#q2').click(function () {
-        $('#a2').fadeToggle();
-    })
+        $('#a2').fadeToggle(300);
+      })
     $('#q3').click(function () {
-        $('#a3').fadeToggle();
+        $('#a3').fadeToggle(300);
     })
     $('#q4').click(function () {
-        $('#a4').fadeToggle();
+        $('#a4').fadeToggle(300);
     })
     $('#q5').click(function(){
-        $('#a5').fadeToggle();
+        $('#a5').fadeToggle(300);
     })
     $('#q6').click(function(){
-        $('#a6').fadeToggle()
+        $('#a6').fadeToggle(300)
     })
     $('#q7').click(function(){
-        $('#a7').fadeToggle()
+        $('#a7').fadeToggle(300)
     })
 
 });
