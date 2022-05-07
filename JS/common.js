6c55877b816e571efda3dfca65bf0dc8b2ca4e8c
tinchin40 <tinchin40@gmail.com>
2022-05-08 03:40:26 +0800

    帳篷rwd完成

--- JS/common.js
+++ JS/common.js
@@ -1,16 +1,18 @@
 // 漢堡
 $(document).ready(function () {
 
-    $('.hamburger').click(function () {
-        $('ul').slideToggle(300);
-        $('button').toggleClass('is-active');
+    $('.hamburger').click(function () { //漢堡按鈕觸發下拉選單
+        
+        $('.nav_ul').slideToggle(300); //下拉選單
+        
+        $('button').toggleClass('is-active'); //增加漢堡動畫
     })
 
     
 
     $('nav li:first-child').click(function () {
         // console.log('sss');
-        $('nav li:first-child ol').slideToggle(300);
+        $('nav li:first-child ol').slideToggle(300); //下拉子選單
     })
 
 
