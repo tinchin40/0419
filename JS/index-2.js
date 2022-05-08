a7c5ed0c775e03227651cca3e8eb3396cfd77450
tinchin40 <tinchin40@gmail.com>
2022-05-08 23:51:34 +0800

    導覽列移除交通指南

--- JS/index-2.js
+++ JS/index-2.js
@@ -1,13 +1,5 @@
-// let big_img = document.getElementById("big_img");
-// let sm_img1 = document.getElementById("sm_img1");
-// let sm_img2 = document.getElementById("sm_img2");
-// let sm_img3 = document.getElementById("sm_img3");
-// let sm_img4 = document.getElementById("sm_img4");
 
-// sm_img1.addEventListener("click", function(){
-//     let sm_img_src = sm_img1.getAttribute("src");
-//     big_img.src = sm_img_src;
-// });
+
 let swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
